ceac599b3d225484554f14c64a5d9968469e95a7
sebasaez <62625595+sebasaez@users.noreply.github.com>
2021-08-23 21:37:13 -0400

    Add files via upload

---
+++ cuenta.c
@@ -0,0 +1,10 @@
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+
+int main(void) {
+    printf(1, "La cantidad de procesos en ejecucion en la CPU es %d\n", getprocs());
+    exit();
+}
